7568f43f461417650a5bdfff6bc001c63d5b3276
Keiji Tokunaga <keizi81@gmail.com>
2022-10-16 01:34:29 +0000

    init commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn get_value_bad(v: bool, result: &mut usize) -> bool {
+    if v {
+        *result = 100;
+        true
+    } else {
+        false
+    }
+}
+
+fn get_value_good(v: bool) -> Option<usize> {
+    if v {
+        Some(100)
+    } else {
+        None
+    }
+}
+
+fn main() {
+    // rustぽくない書き方
+    let mut result = 0;
+    if get_value_bad(true, &mut result) {
+        println!("success: {}", result);
+    } else {
+        println!("failure");
+    }
+
+    // rustぽい書き方
+    match get_value_good(true) {
+        Some(result) => println!("success: {}", result),
+        None => println!("failure"),
+    }
+}
